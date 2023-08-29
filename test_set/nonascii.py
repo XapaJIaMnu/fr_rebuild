90d83405c14e287a14356b9b16d299a996ae4ae1
XapaJIaMnu <nheart@gmail.com>
2023-08-29 10:30:47 +0100

    Lower sequence lengths in noise so we don't run out of memory

--- test_set/nonascii.py
+++ test_set/nonascii.py
@@ -4,6 +4,6 @@ import sys
 for line in sys.stdin:
     newline = ""
     for char in line:
-        if ord(char) > 127:
+        if ord(char) > 255:
             newline = newline + char
     print(newline)
